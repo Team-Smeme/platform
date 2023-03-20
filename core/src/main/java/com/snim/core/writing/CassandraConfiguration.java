515e01dfe19753a99bec0806c653a5221e01a98a
머환임 <udsward@gmail.com>
2023-03-20 22:38:28 +0900

    set Cassandra for Writing Domain Configuration

--- core/src/main/java/com/snim/core/writing/CassandraConfiguration.java
+++ core/src/main/java/com/snim/core/writing/CassandraConfiguration.java
@@ -1,8 +1,7 @@
-package com.snim.configuration.cassandra;
+package com.snim.core.writing;
 
 import com.datastax.oss.driver.api.core.CqlSession;
-import com.snim.configuration.SeparatedProfileConfigurable;
-import com.snim.core.CoreBasePackage;
+import com.snim.configuration.CassandraSeparatedProfileConfigurable;
 import org.cassandraunit.CQLDataLoader;
 import org.cassandraunit.dataset.cql.ClassPathCQLDataSet;
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.data.cassandra.config.SchemaAction;
 import org.springframework.data.cassandra.config.SessionFactoryFactoryBean;
@@ -27,17 +25,16 @@ import java.io.IOException;
 import java.util.Objects;
 
 @Configuration
-@EnableCassandraRepositories(basePackageClasses = CoreBasePackage.class)
-public class SessionConfiguration implements SeparatedProfileConfigurable<CqlSession> {
-    private static final String CQL_SESSION_BEAN = "cqlSessionBean";
-    private static final String CQL_SESSION_FACTORY_BEAN = "cqlSessionFactoryBean";
-    private static final String SESSION_FACTORY_FACTORY_BEAN = "sessionFactoryFactoryBean";
-    private static final String CASSANDRA_MAPPING_CONTEXT = "cassandraMappingContext";
-    private static final String CASSANDRA_CONVERTER = "cassandraConverter";
-    private static final String CASSANDRA_OPERATIONS = "cassandraOperations";
+@EnableCassandraRepositories(basePackageClasses = WritingCorePackage.class)
+public class CassandraConfiguration implements CassandraSeparatedProfileConfigurable {
+    private static final String WRITING_CQL_SESSION_BEAN = "writingCqlSessionBean";
+    private static final String WRITING_SESSION_FACTORY_FACTORY_BEAN = "writingSessionFactoryFactoryBean";
+    private static final String WRITING_CASSANDRA_MAPPING_CONTEXT = "writingCassandraMappingContext";
+    private static final String WRITING_CASSANDRA_CONVERTER = "writingCassandraConverter";
+    private static final String WRITING_CASSANDRA_OPERATIONS = "writingCassandraOperations";
 
     @Override
-    @Bean(CQL_SESSION_BEAN)
+    @Bean(WRITING_CQL_SESSION_BEAN)
     public CqlSession embedded() throws IOException, InterruptedException {
         EmbeddedCassandraServerHelper.startEmbeddedCassandra();
         CqlSession session = EmbeddedCassandraServerHelper.getSession();
@@ -53,7 +50,7 @@ public class SessionConfiguration implements SeparatedProfileConfigurable<CqlSes
         return cqlSessionFactoryBean.getObject();
     }
 
-    @Bean(CASSANDRA_MAPPING_CONTEXT)
+    @Bean(WRITING_CASSANDRA_MAPPING_CONTEXT)
     @Primary
     public CassandraMappingContext cassandraMappingContext() {
         CassandraMappingContext cassandraMappingContext = new CassandraMappingContext();
@@ -64,20 +61,20 @@ public class SessionConfiguration implements SeparatedProfileConfigurable<CqlSes
         return cassandraMappingContext;
     }
 
-    @Bean(CASSANDRA_CONVERTER)
+    @Bean(WRITING_CASSANDRA_CONVERTER)
     @Primary
     public CassandraConverter cassandraConverter(
-            @Qualifier(CASSANDRA_MAPPING_CONTEXT) CassandraMappingContext cassandraMappingContext
+            @Qualifier(WRITING_CASSANDRA_MAPPING_CONTEXT) CassandraMappingContext cassandraMappingContext
     ) {
         return new MappingCassandraConverter(cassandraMappingContext);
     }
 
-    @Profile({"test", "local"})
-    @Bean(SESSION_FACTORY_FACTORY_BEAN)
+    @Bean(WRITING_SESSION_FACTORY_FACTORY_BEAN)
     @Primary
+    @Override
     public SessionFactoryFactoryBean SessionFactoryFactoryBeanForCreateIfNotExists(
-            @Qualifier(CQL_SESSION_BEAN) CqlSession session,
-            @Qualifier(CASSANDRA_CONVERTER) CassandraConverter cassandraConverter
+            @Qualifier(WRITING_CQL_SESSION_BEAN) CqlSession session,
+            @Qualifier(WRITING_CASSANDRA_CONVERTER) CassandraConverter cassandraConverter
     ) {
         SessionFactoryFactoryBean sessionFactoryFactoryBean = new SessionFactoryFactoryBean();
 
@@ -88,12 +85,12 @@ public class SessionConfiguration implements SeparatedProfileConfigurable<CqlSes
         return sessionFactoryFactoryBean;
     }
 
-    @Profile({"!test", "!local"})
-    @Bean(SESSION_FACTORY_FACTORY_BEAN)
+    @Bean(WRITING_SESSION_FACTORY_FACTORY_BEAN)
     @Primary
+    @Override
     public SessionFactoryFactoryBean SessionFactoryFactoryBeanForNone(
-            @Qualifier(CQL_SESSION_BEAN) CqlSession session,
-            @Qualifier(CASSANDRA_CONVERTER) CassandraConverter cassandraConverter
+            @Qualifier(WRITING_CQL_SESSION_BEAN) CqlSession session,
+            @Qualifier(WRITING_CASSANDRA_CONVERTER) CassandraConverter cassandraConverter
     ) {
         SessionFactoryFactoryBean sessionFactoryFactoryBean = new SessionFactoryFactoryBean();
 
@@ -104,11 +101,11 @@ public class SessionConfiguration implements SeparatedProfileConfigurable<CqlSes
         return sessionFactoryFactoryBean;
     }
 
-    @Bean(CASSANDRA_OPERATIONS)
+    @Bean(WRITING_CASSANDRA_OPERATIONS)
     @Primary
     public CassandraOperations cassandraTemplate(
-            @Qualifier(SESSION_FACTORY_FACTORY_BEAN) SessionFactoryFactoryBean sessionFactory,
-            @Qualifier(CASSANDRA_CONVERTER) CassandraConverter cassandraConverter
+            @Qualifier(WRITING_SESSION_FACTORY_FACTORY_BEAN) SessionFactoryFactoryBean sessionFactory,
+            @Qualifier(WRITING_CASSANDRA_CONVERTER) CassandraConverter cassandraConverter
     ) throws Exception {
         return new CassandraTemplate(
                 Objects.requireNonNull(sessionFactory.getObject()) // DefaultSessionFactory
